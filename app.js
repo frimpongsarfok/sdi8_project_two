da8c6fdca6efb99ff516c49b58cc9706b00ede8c
Sarfo K. Frimpong <frimpongsarfok@gmail.com>
2024-04-22 09:03:39 -0400

    added fish icon url

--- app.js
+++ app.js
@@ -53,6 +53,29 @@ app.get("/image/:imageNum",(req,res)=>{
 
 })
 
+app.get("/image/icon/fish/:imageNum",(req,res)=>{    
+  const imageNume=req.params.imageNum;
+  fs.readFile('fish.json', 'utf8', (err, data) => {
+      if (err) {
+        res.status(500).send('Error reading file');
+        return;
+      }
+  
+      try {
+        const jsonData = JSON.parse(data);
+        const fishName=jsonData[imageNume]["file-name"];
+        console.log('Files in the directory:', fishName);
+
+        res.setHeader('Content-Type', 'image/jpeg');
+        const imagePath = path.join(__dirname, 'fish', fishName+".png");
+        res.sendFile(imagePath);
+        console.log('Files in the directory:', imagePath);
+      } catch (err) {
+        res.status(500).send('Error parsing JSON:'+err);
+      }
+    });
+
+})
 app.listen(process.env.PORT ||port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
